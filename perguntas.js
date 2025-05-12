3299dce45a9f0b062bdb1d229c361bc7339b346e
amandaascheid <scheid.amanda@escola.pr.gov.br>
2025-05-12 15:02:51 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criarCartao(
+    'Programação',
+    'O que é JavaScript',
+    'É uma linguagem de programação'
+)
+
+criarCartao(
+    'Inglês',
+    'Como se diz "Dia do pagamento" em inglês?',
+    'Payday'
+)
+
+criarCartao(
+    categoria,
+    pergunta,
+    resposta
+)
