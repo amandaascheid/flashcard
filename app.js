d58c93ad50cf4d3301898e0b095fe9921bcd8a8b
amandaascheid <scheid.amanda@escola.pr.gov.br>
2025-06-09 14:30:20 +0000

    ok

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-function criaCartao(categoria, pergunta, resposta) {
-    let container = document
+function criarCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
 let cartao = document.createElement('article')
 cartao.className = 'cartao'
 cartao.innerHTML =`
 <article class="cartao">
             <div class="cartao__conteudo">
-                <h3>Programação</h3>
+                <h3>${categoria}</h3>
                 <div class="cartao__conteudo__pergunta">
-                    <p>O que é JS?</p>
+                    <p>${pergunta}</p>
                 </div>
                 <div class="cartao__conteudo__resposta">
-                    <p> É uma linguagem de programação.</p>
+                    <p>${resposta}</p>
                 </div>
             </div>
         </article>
 `
+let respostaEstaVisivel = false
+function viraCartao(){
+    respostaEstaVisivel = !
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
 container.appendChild(cartao)
 }
+
--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,35 @@
-criarCartao(
+criarCartao (
     'Programação',
     'O que é JavaScript',
-    'É uma linguagem de programação'
+    'É uma linguagem de programação.'
 )
 
-criarCartao(
+criarCartao (
     'Inglês',
     'Como se diz "Dia do pagamento" em inglês?',
-    'Payday'
+    'Payday.'
 )
 
-criarCartao(
-    categoria,
-    pergunta,
-    resposta
+criarCartao (
+    'Matemática',
+    'Quem é o pai da matemática?',
+    'Arquimedes.'
+)
+
+criarCartao (
+    'História',
+    'Em quais anos foram a primeira guerra mundial?',
+    '1914 até 1918.'
+)
+
+criarCartao (
+    'Geografia',
+    'Qual o país com a maior população do mundo?',
+    'Índia.'
+)
+
+criarCartao (
+    'Física',
+    'Qual é a unidade de medida para velocidade?',
+    'Metros por segundo (m/s).'
 )
